29e72b74f52fe89bf22e43801c58c389b321fe49
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2024-06-20 13:59:19 +0200

    feat: add babel config

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,16 +3,4 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-  optimization: {
-    minimizer: [`...`],
-  },
 });
